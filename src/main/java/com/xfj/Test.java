7870a5e0aa2986a254d2942885fc71a805a13691
suntf <1002142758@qq.com>
2017-12-22 19:15:57 +0800

    测试

---
+++ src/main/java/com/xfj/Test.java
@@ -0,0 +1,26 @@
+package com.xfj;
+
+import java.util.Map;
+
+/**
+ * @author suntf
+ * @date 17/12/22
+ */
+public class Test
+{
+
+
+     /**
+      *
+      * @author suntf 
+      * @date 2017-12-22
+      * @param 
+      * @return 
+      */ 
+    public String sayHello(String name,Map map)
+    {
+        System.out.println(name);
+        System.out.println("cc");
+        return  "c";
+    }
+}
